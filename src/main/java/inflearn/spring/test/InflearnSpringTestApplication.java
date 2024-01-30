32ba3ee1a3cea1d76c0df606a1f602b247fe427f
Yoo <hiyoojeong@gmail.com>
2024-01-30 21:07:18 +0900

    :tada: feat: 프로젝트 생성

---
+++ src/main/java/inflearn/spring/test/InflearnSpringTestApplication.java
@@ -0,0 +1,13 @@
+package inflearn.spring.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflearnSpringTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InflearnSpringTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/inflearn/spring/test/InflearnSpringTestApplicationTests.java
@@ -0,0 +1,13 @@
+package inflearn.spring.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflearnSpringTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
