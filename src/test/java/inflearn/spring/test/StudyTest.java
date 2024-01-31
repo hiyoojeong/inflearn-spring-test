e5e472e6507b734c3750baa64bc6e2f8d674cea1
Yoo <hiyoojeong@gmail.com>
2024-01-31 16:03:50 +0900

    :sparkles: feat: JUnit5 테스트 반복하기 (1)

--- src/test/java/inflearn/spring/test/StudyTest.java
+++ src/test/java/inflearn/spring/test/StudyTest.java
@@ -9,6 +9,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.RepetitionInfo;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class StudyTest {
@@ -24,6 +28,20 @@ class StudyTest {
   @DisplayName("스터디 만들기 slow")
   void create2() {
     System.out.println("second create");
+  }
+
+  @DisplayName("스터디 만들기")
+  @RepeatedTest(value = 10, name = "{displayName}, {currentRepetition}/{totalRepetitions}")
+  void repeatTest(RepetitionInfo repetitionInfo) {
+    System.out.println("test" + repetitionInfo.getCurrentRepetition() + "/"
+        + repetitionInfo.getTotalRepetitions());
+  }
+
+  @DisplayName("스터디 만들기")
+  @ParameterizedTest(name = "{index} {displayName} message={0}")
+  @ValueSource(strings = {"날씨가", "많이", "추워지고", "있네요"})
+  void parameterizedTest(String message) {
+    System.out.println(message);
   }
 
   // 모든 테스트를 실행하기 이전에 딱 한번만 호출된다.
