ba5b4ac4b3b656b8a5e19bc7cc6ec2b3e0a9e067
Yoo <hiyoojeong@gmail.com>
2024-01-31 16:26:16 +0900

    :sparkles: feat: JUnit5 테스트 반복하기 (2) - 커스텀 클래스 다중인자 받아오기

--- src/test/java/inflearn/spring/test/StudyTest.java
+++ src/test/java/inflearn/spring/test/StudyTest.java
@@ -12,10 +12,16 @@ import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.RepetitionInfo;
+import org.junit.jupiter.api.extension.ParameterContext;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.aggregator.AggregateWith;
+import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
+import org.junit.jupiter.params.aggregator.ArgumentsAggregationException;
+import org.junit.jupiter.params.aggregator.ArgumentsAggregator;
 import org.junit.jupiter.params.converter.ArgumentConversionException;
 import org.junit.jupiter.params.converter.ConvertWith;
 import org.junit.jupiter.params.converter.SimpleArgumentConverter;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
@@ -43,9 +49,18 @@ class StudyTest {
 
   @DisplayName("스터디 만들기")
   @ParameterizedTest(name = "{index} {displayName} message={0}")
-  @ValueSource(ints = {10, 20, 40})
-  void parameterizedTest(@ConvertWith(StudyConvertor.class) Study study) {
-    System.out.println(study.getLimit());
+  @CsvSource({"10, '자바 스터디'", "20, '스프링'"})
+  void parameterizedTest(@AggregateWith(StudyAggregator.class) Study study) {
+    System.out.println(study);
+  }
+
+  static class StudyAggregator implements ArgumentsAggregator {
+
+    @Override
+    public Object aggregateArguments(ArgumentsAccessor accessor, ParameterContext context)
+        throws ArgumentsAggregationException {
+      return new Study(accessor.getInteger(0), accessor.getString(1));
+    }
   }
 
   static class StudyConvertor extends SimpleArgumentConverter {
