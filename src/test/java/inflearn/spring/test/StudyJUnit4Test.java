04ce695fbfa9bd63b6b85e6ae13135ba604a85a8
Yoo <hiyoojeong@gmail.com>
2024-01-31 22:40:45 +0900

    :sparkles: feat: JUnit5 JUnit4 마이그레이션

---
+++ src/test/java/inflearn/spring/test/StudyJUnit4Test.java
@@ -0,0 +1,22 @@
+package inflearn.spring.test;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class StudyJUnit4Test {
+
+  @Before
+  public void before() {
+    System.out.println("before");
+  }
+
+  @Test
+  public void createTest1() {
+    System.out.println("test1");
+  }
+
+  @Test
+  public void createTest2() {
+    System.out.println("test2");
+  }
+}
